3c138ed088db8350de85d2e1a4d7e41fb6b08cdf
kahmadnia1375-glitch <k.ahmadnia1375@gmail.com>
2026-09-01 22:51:37 +0200

    Update 01_Load_Data.R

--- R/01_Load_Data.R
+++ R/01_Load_Data.R
@@ -1,1 +1,22 @@
+library(Seurat)
 
+url <- "https://cf.10xgenomics.com/samples/cell-exp/1.1.0/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz"
+
+download.file(
+  url,
+  destfile = "pbmc3k.tar.gz",
+  mode = "wb"
+)
+
+untar("pbmc3k.tar.gz")
+
+data <- Read10X("filtered_gene_bc_matrices/hg19/")
+
+pbmc <- CreateSeuratObject(
+  counts = data,
+  project = "PBMC3K",
+  min.cells = 3,
+  min.features = 200
+)
+
+pbmc
