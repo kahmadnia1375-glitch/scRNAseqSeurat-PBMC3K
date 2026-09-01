27acfeb32afa01f15c7ab6b34d44b1f9c6574660
kahmadnia1375-glitch <k.ahmadnia1375@gmail.com>
2026-09-01 22:56:03 +0200

    Update 02_QC.R

--- R/02_QC.R
+++ R/02_QC.R
@@ -1,6 +1,3 @@
-library(Seurat)
-library(ggplot2)
-
 pbmc[["percent.mt"]] <- PercentageFeatureSet(
   pbmc,
   pattern = "^MT-"
@@ -11,3 +8,12 @@ VlnPlot(
   features = c("nFeature_RNA", "nCount_RNA", "percent.mt"),
   ncol = 3
 )
+
+pbmc <- subset(
+  pbmc,
+  subset = nFeature_RNA > 200 &
+           nFeature_RNA < 2500 &
+           percent.mt < 5
+)
+
+pbmc
