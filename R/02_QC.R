db1854b4d866d6bea13d55b26767d00548bfde13
kahmadnia1375-glitch <k.ahmadnia1375@gmail.com>
2026-09-01 22:52:27 +0200

    Update 02_QC.R

--- R/02_QC.R
+++ R/02_QC.R
@@ -1,1 +1,13 @@
+library(Seurat)
+library(ggplot2)
 
+pbmc[["percent.mt"]] <- PercentageFeatureSet(
+  pbmc,
+  pattern = "^MT-"
+)
+
+VlnPlot(
+  pbmc,
+  features = c("nFeature_RNA", "nCount_RNA", "percent.mt"),
+  ncol = 3
+)
