f8a23b48c028412b0cf485527d92787ec6014d78
Kosar Ahmadnia <k.ahmadnia1375@gmail.com>
2026-09-02 19:26:04 +0200

    Update 09_Cell_Annotation.R

--- R/R/R/09_Cell_Annotation.R
+++ R/R/R/09_Cell_Annotation.R
@@ -1,3 +1,6 @@
+library(Seurat)
+library(dplyr)
+
 markers <- FindAllMarkers(
   pbmc,
   only.pos = TRUE,
@@ -5,4 +8,29 @@ markers <- FindAllMarkers(
   logfc.threshold = 0.25
 )
 
-head(markers)
+celltype_annotations <- c(
+  "0" = "Naive/Memory T cells",
+  "1" = "Classical monocytes",
+  "2" = "CD4 T cells",
+  "3" = "B cells",
+  "4" = "CD8 T cells",
+  "5" = "FCGR3A+ monocytes",
+  "6" = "NK cells",
+  "7" = "CD1C+ dendritic cells",
+  "8" = "Platelets"
+)
+
+pbmc <- RenameIdents(
+  pbmc,
+  celltype_annotations
+)
+
+pbmc$cell_type <- Idents(pbmc)
+
+DimPlot(
+  pbmc,
+  reduction = "umap",
+  group.by = "cell_type",
+  label = TRUE,
+  repel = TRUE
+)
