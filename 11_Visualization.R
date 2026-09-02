03ca357d445f668b951a51dbf8a198f9ea44d83d
Kosar Ahmadnia <k.ahmadnia1375@gmail.com>
2026-09-02 20:53:01 +0200

    Update 11_Visualization.R

--- 11_Visualization.R
+++ 11_Visualization.R
@@ -1,1 +1,15 @@
+library(Seurat)
+library(dplyr)
 
+top_markers <- deg %>%
+  group_by(cluster) %>%
+  slice_max(
+    order_by = avg_log2FC,
+    n = 5
+  )
+
+DoHeatmap(
+  pbmc,
+  features = top_markers$gene,
+  group.by = "cell_type"
+)
